0c0293bbb8732e9d4d7f5a9320513e6baf45992e
alvaro <alvaro_enrique_1997@hotmail.com>
2019-03-21 18:15:37 -0500

    se agraga el api para subir imagenes
    se agrega imagenes a la carpeta local ./files/

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ import (
 func main()  {
         e := echo.New()
 
+        /*
+        server, err := socketio.NewServer(nil)
+        if err != nil {
+                log.Fatal(err)
+        }
+        */
+
+        e.Static("/", "public")
+
         e.POST("/register", controllers.CreateUser)
         e.POST("/login", controllers.LoginUser)
         e.POST("/comment", controllers.CommentCreate)
         e.GET("/comment", controllers.CommentGetAll)
+        e.POST("/upload", controllers.Upload)
         e.Start(":80")
 }
